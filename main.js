02478dca85b49cf50b2067a10ca62b41a32e61d1
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-07-28 23:40:56 +0100

    stars

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,13 +19,45 @@ camera.position.setZ(30);
 renderer.render( scene, camera);
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true } );
+const material = new THREE.MeshStandardMaterial( {color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
 
+const pointLight = new THREE.PointLight(0xffffff, 1000)
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff})
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+    const star = new THREE.Mesh(geometry, material);
+    star.position.set(x, y, z);
+    scene.add(star)
+
+}
+
+Array(800).fill().forEach(addStar)
+
 function animate() {
     requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
     renderer.render( scene, camera );
 }
 
